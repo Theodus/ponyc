a66063d20fda40ee3013083d7c22995248f67ddd
theodus <theodusbutler@gmail.com>
2019-01-13 15:10:38 -0500

    improve todo.bash

--- todo.bash
+++ todo.bash
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# https://github.com/ponylang/ponyc/issues/267
+
 # TODO:
 # - guard all relevant ast_id switch statement defaults
 # - use TK_TYPEALIAS as resolved branch
 # - use alias for error messages
+# - remove `case TK_TYPEALIAS: pony_assert(0)`
+# - use alias for docgen
 # - allow finite recursive type aliases
 
-make -f Makefile-lib-llvm config=debug lto=yes LTO_PLUGIN=/usr/lib/LLVMgold.so -j8 \
-  && ./build/debug/libponyc.tests --gtest_filter=BadPonyTest.*
+make -f Makefile-lib-llvm config=debug lto=yes LTO_PLUGIN=/usr/lib/LLVMgold.so -j8
+
+if [[ $? == 0 ]]; then
+  # ./build/debug/libponyc.tests --gtest_filter=BadPonyTest.*
+  ./build/debug/ponyc -r flatten packages/stdlib
+fi
